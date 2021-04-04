c730f77db4b50147a7d06ab90689ceb1fa237737
Tajammal <tajammalmaqbool11@gmail.com>
2021-04-04 13:55:05 -0700

    Project

--- static/js/loginform.js
+++ static/js/loginform.js
@@ -3,17 +3,25 @@ function checkid() {
     var mypin = document.getElementById("exampleInputPassword").value;
     if (myemail == "") {
         document.getElementById("exampleInputEmail").style.borderColor = "red";
+        document.getElementById("email-valid").style.display = "block";
+        document.getElementById("email-valid").textContent="Looks Bad!!";
+        document.getElementById("email-valid").classList = "text-danger";
     } else {
         document.getElementById("exampleInputEmail").style.borderColor = "#d1d3e2";
+        document.getElementById("email-valid").style.display = "none";
     }
     if (mypin == "") {
         document.getElementById("exampleInputPassword").style.borderColor = "red";
         document.getElementById("button12").style.borderColor = "red";
+        document.getElementById("pin-valid").style.display = "block";
+        document.getElementById("pin-valid").textContent="Looks Bad!!";
+        document.getElementById("pin-valid").classList = "text-danger";
     }
     else {
         document.getElementById("exampleInputPassword").style.borderColor = "#d1d3e2";
         document.getElementById("button12").style.borderColor = "#d1d3e2";
-    }
+        document.getElementById("pin-valid").style.display = "none";
+    }  
 }
 
 function showpassword(){
